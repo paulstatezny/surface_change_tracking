c01a3eba3ebbddca07ee21b6e09eaeae8160a88b
Paul Statezny <paulstatezny@gmail.com>
2021-07-14 10:07:00 -0700

    Add Surface

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule SurfaceChangeTracking.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:surface, "~> 0.5.1"}
     ]
   end
 
