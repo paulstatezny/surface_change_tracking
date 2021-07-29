2b88ba354fec68d11b0f23d5d14b61e58097a7d9
Paul Statezny <paulstatezny@gmail.com>
2021-07-28 19:51:34 -0700

    Document issue with Forms backed by Changesets

--- lib/surface_change_tracking_web/live/form_changeset_live.ex
+++ lib/surface_change_tracking_web/live/form_changeset_live.ex
@@ -29,6 +29,30 @@ defmodule SurfaceChangeTrackingWeb.FormChangesetLive do
         <Input label="Name" />
       </FieldContext>
     </Form>
+
+    <ul style="max-width: 640px; font-size: 18px">
+      <li>
+        This example has <code>&lt;Form for=&#123;@changeset&#125; change="change"&gt;</code>
+      </li>
+
+      <li>
+        There is also an <code>Input</code> component, which renders a
+        <code>Surface.Components.Form.TextInput</code> and a label passed in as a prop.
+      </li>
+
+      <li>
+        The change event handler simply updates <code>assigns.changeset</code>
+        using the input data.
+      </li>
+
+      <li>
+        Note that when the change event fires (by typing in the input), the
+        label "Name" is sent in the set of changes even though it hasn't
+        changed, and even though none of the LiveEEx Pitfalls outlined in the
+        <a target="_blank" href="https://hexdocs.pm/phoenix_live_view/assigns-eex.html#liveeex-pitfalls">official guide</a>
+        have been violated.
+      </li>
+    </ul>
     """
   end
 
